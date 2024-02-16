550dee4e2e1bb59d0183d3b80e26f8e65599efcf
HodgkinsDev <41766856+HodgkinsDev@users.noreply.github.com>
2024-02-16 03:25:46 -0500

    another bugfix startup.lua

--- startup.lua
+++ startup.lua
@@ -1,7 +1,10 @@
-local readOnlyPaths = {"test/"}
+local readOnlyPaths = {}
 local old_fsOpen = _G["fs"]["open"]
 local old_fsIsReadOnly = _G["fs"]["isReadOnly"]
 local old_fsDelete = _G["fs"]["delete"]
+local old_fsMove = _G["fs"]["move"]
+local old_fsCopy = _G["fs"]["copy"]
+local old_fsWrite = _G["fs"]["write"]
 
 -- Helper function to normalize paths
 local function normalizePath(path)
@@ -55,6 +58,62 @@ _G["fs"]["delete"] = function(path)
         return nil
     else
         return old_fsDelete(path)
+    end
+end
+
+_G["fs"]["move"] = function(fromPath, toPath)
+    local isFromReadOnly = false
+    local isToReadOnly = false
+    fromPath = normalizePath(fromPath)
+    toPath = normalizePath(toPath)
+    for _, readOnlyPath in ipairs(readOnlyPaths) do
+        if fromPath:sub(1, #normalizePath(readOnlyPath)) == normalizePath(readOnlyPath) then
+            isFromReadOnly = true
+        end
+        if toPath:sub(1, #normalizePath(readOnlyPath)) == normalizePath(readOnlyPath) then
+            isToReadOnly = true
+        end
+    end
+    if isFromReadOnly or isToReadOnly then
+        return nil
+    else
+        return old_fsMove(fromPath, toPath)
+    end
+end
+
+_G["fs"]["copy"] = function(fromPath, toPath)
+    local isFromReadOnly = false
+    local isToReadOnly = false
+    fromPath = normalizePath(fromPath)
+    toPath = normalizePath(toPath)
+    for _, readOnlyPath in ipairs(readOnlyPaths) do
+        if fromPath:sub(1, #normalizePath(readOnlyPath)) == normalizePath(readOnlyPath) then
+            isFromReadOnly = true
+        end
+        if toPath:sub(1, #normalizePath(readOnlyPath)) == normalizePath(readOnlyPath) then
+            isToReadOnly = true
+        end
+    end
+    if isFromReadOnly or isToReadOnly then
+        return nil
+    else
+        return old_fsCopy(fromPath, toPath)
+    end
+end
+
+_G["fs"]["write"] = function(path, text)
+    local isReadOnlyPath = false
+    path = normalizePath(path)
+    for _, readOnlyPath in ipairs(readOnlyPaths) do
+        if path:sub(1, #normalizePath(readOnlyPath)) == normalizePath(readOnlyPath) then
+            isReadOnlyPath = true
+            break
+        end
+    end
+    if isReadOnlyPath then
+        return nil
+    else
+        return old_fsWrite(path, text)
     end
 end
 
